982399e1bd440320b3e5fdff9f8b226e9e4b8173
marion <marion>
2002-09-10 23:18:17 +0000

    Fixed log file format error due to new item link feature. Completely revamped parser core for extra speed.

--- fraglets/java/net/sourceforge/fraglets/yaelp/EqlogParser.java
+++ fraglets/java/net/sourceforge/fraglets/yaelp/EqlogParser.java
@@ -5,8 +5,7 @@
 
 package net.sourceforge.fraglets.yaelp;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedReader;
+import java.io.Reader;
 import java.io.InputStreamReader;
 import java.io.IOException;
 import java.io.File;
@@ -34,7 +33,7 @@ import java.util.zip.GZIPInputStream;
  * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  *
  * @author marion@users.sourceforge.net
- * @version $Revision: 1.9 $
+ * @version $Revision: 1.10 $
  */
 public class EqlogParser {
     public static final String PREFIX = "[Sun Apr 01 16:38:57 2001] ";
@@ -43,31 +42,107 @@ public class EqlogParser {
     public static final int TIMESTAMP_END = PREFIX.length()-2;
     public static final int LINE_START = PREFIX.length();
     
-    protected BufferedReader in;
+    protected Reader in;
+    
+    protected char buffer[];
+    
+    protected int off;
+    
+    protected int len;
+    
+    protected int count;
     
     /** Creates new Parser */
-    public EqlogParser(BufferedReader in)
+    public EqlogParser(Reader in)
     {
         this.in = in;
+        this.buffer = new char[8192];
     }
     
-    public synchronized Line readLine()
+    /** Fill the buffer with more characters. Grow buffer as needed.
+     * @return true iff more characters were read.
+     * @throws IOException propagated from Reader.read
+     */
+    protected boolean fill()
         throws IOException
     {
-        String line = in.readLine();
-        if (line == null) {
-            return null;
-        } else  try {
-            String timestamp = line.substring(TIMESTAMP_START, TIMESTAMP_END);
-            char rest[] = line.substring(LINE_START).toCharArray();
-            return new Line(timestamp, rest);
-        } catch (StringIndexOutOfBoundsException ex) {
-            // FIXME quickhack
-            if (line.length() == 0 ||
-                line.startsWith(" got ") && line.endsWith(".")) {
-                return readLine();
+        if (off + len >= buffer.length) {
+            if (off > 0) {
+                System.arraycopy(buffer, off, buffer, 0, len);
+                off = 0;
+            } else {
+                char grow[] = new char[buffer.length + buffer.length / 2];
+                System.arraycopy(buffer, off, grow, 0, len);
+                buffer = grow;
+                off = 0;
             }
-            throw new IOException("invalid log file format");
+        }
+        
+        int fill = off + len;
+        int n = in.read(buffer, fill, buffer.length - fill);
+        if (n > 0) {
+            len += n;
+            return true;
+        } else {
+            return false;
+        }
+    }
+    
+    public synchronized boolean readLine(Line line)
+        throws IOException
+    {
+        char c;
+        int scan = off;
+        int last = scan;
+        int end = off + len;
+        boolean newline = false;
+        char buffer[] = this.buffer;
+        search: for(;;) {
+            if (scan >= end) {
+                scan = scan - off;
+                if (!fill()) {
+                    if (scan == 0) {
+                        return false; // end of file
+                    }
+                    break search;
+                }
+                scan = off + scan;
+                end = off + len;
+            }
+            switch (buffer[scan++]) {
+                case '\n':
+                    count += 1;
+                    // fall through
+                case '\r':
+                    newline = true;
+                    continue;
+                case '[':
+                    if (newline) {
+                        scan = scan - off - 1;
+                        break search;
+                    }
+                    // fall through
+                default:
+                    newline = false;
+                    last = scan;
+                    continue;
+            }
+        }
+        
+        int start = off;
+        
+        len -= scan;
+        off += scan;
+        scan = last - start;
+        
+        if (last - start >= LINE_START) {
+            String timestamp = new String(buffer, start + TIMESTAMP_START, TIMESTAMP_END - TIMESTAMP_START);
+            char rest[] = new char[scan - LINE_START];
+            System.arraycopy(buffer, start + LINE_START, rest, 0, scan - LINE_START);
+            line.recycle(timestamp, rest);
+            return true;
+        } else {
+            throw new IOException("invalid log file format :'"+new String(buffer, start, scan)+"'");
         }
     }
     
@@ -80,35 +155,32 @@ public class EqlogParser {
     public static int parseFile(File file, Recognizer recognizer)
         throws IOException
     {
-        BufferedReader in;
+        Reader in;
         if (file.getName().endsWith(".gz")) {
             System.out.println("reading compressed "+file+"...");
-            in = new BufferedReader(new InputStreamReader(new GZIPInputStream
-                (new FileInputStream(file))));
+            in = new InputStreamReader(new GZIPInputStream
+                (new FileInputStream(file)));
         } else {
             System.out.println("reading text "+file+"...");
-            in = new BufferedReader(new FileReader(file));
+            in = new FileReader(file);
         }
         EqlogParser parser = new EqlogParser(in);
-        Line line;
+        Line line = new Line(0L, (char[])null);
         int n = 0;
-        int i = 0;
         try {
-            do {
-                line = parser.readLine();
-                if (line == null) {
-                    // end
-                } else if (recognizer.parse(line)) {
+            for (;;) {
+                if (parser.readLine(line)) {
                     n++;
+                    recognizer.parse(line);
                 } else {
-                    i++;
+                    break;
                 }
-            } while (line != null);
+            }
             recognizer.setChanged(n > 0 || recognizer.isChanged());
         } catch (IOException ex) {
-            throw new IOException(file + ":" + (n+i) + ": " + ex.getMessage());
+            throw new IOException(file + ":" + parser.getLine() + ": " + ex.getMessage());
         }
-        return n + i;
+        return parser.getLine();
     }
     
     public static void main(String args[])
@@ -142,4 +214,12 @@ public class EqlogParser {
             ex.printStackTrace();
         }
     }
+    
+    /** Getter for property line.
+     * @return Value of property line.
+     */
+    public int getLine() {
+        return this.count;
+    }
+    
 }
