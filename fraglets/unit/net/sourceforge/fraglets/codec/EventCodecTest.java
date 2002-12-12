e0a68dbf818201995d28f39604b290ec916f302c
marion <marion>
2002-12-12 10:34:59 +0000

    Fix formatting.

--- fraglets/unit/net/sourceforge/fraglets/codec/EventCodecTest.java
+++ fraglets/unit/net/sourceforge/fraglets/codec/EventCodecTest.java
@@ -10,124 +10,132 @@ import junit.framework.TestCase;
  * @version $Revision$
  */
 public class EventCodecTest extends TestCase {
-	protected EventCodec testCodec;
+    protected EventCodec testCodec;
 
-	/**
-	 * Constructor for EventCodecTest2.
-	 * @param arg0
-	 */
-	public EventCodecTest(String arg0) {
-		super(arg0);
-	}
+    /**
+     * Constructor for EventCodecTest2.
+     * @param arg0
+     */
+    public EventCodecTest(String arg0) {
+        super(arg0);
+    }
 
-	/**
-	 * Lazily create a text codec fixture.
-	 * 
-	 * @return the test codec fixture
-	 */
-	protected TestCodec getTestCodec() {
-		if (this.testCodec == null) {
-			this.testCodec = new TestCodec();
-		}
-		return (TestCodec)this.testCodec;
-	}
+    /**
+     * Lazily create a text codec fixture.
+     * 
+     * @return the test codec fixture
+     */
+    protected TestCodec getTestCodec() {
+        if (this.testCodec == null) {
+            this.testCodec = new TestCodec();
+        }
+        return (TestCodec) this.testCodec;
+    }
 
-	/**
-	 * Test the fixture.
-	 */
-	public void testEventCodec() {
-		assertNotNull("constructor", getTestCodec());
-	}
+    /**
+     * Test the fixture.
+     */
+    public void testEventCodec() {
+        assertNotNull("constructor", getTestCodec());
+    }
 
-	/**
-	 * Test for reset().
-	 */
-	public void testReset() {
-		TestCodec codec = getTestCodec();
-		int code1 = codec.createCode();
-		testCodec.reset();
-		int code2 = codec.createCode();
-		assertEquals("same code after reset", code1, code2);
-		
-	}
+    /**
+     * Test for reset().
+     */
+    public void testReset() {
+        TestCodec codec = getTestCodec();
+        int code1 = codec.createCode();
+        testCodec.reset();
+        int code2 = codec.createCode();
+        assertEquals("same code after reset", code1, code2);
 
-	/**
-	 * Test for getFirstLiteral().
-	 */
-	public void testGetFirstLiteral() {
-		TestCodec codec = getTestCodec();
-		assertTrue("first literal <= created code", codec.getFirstLiteral() <= codec.createCode());
-	}
+    }
 
-	/**
-	 * Test for getString(int).
-	 */
-	public void testGetString() {
-		TestCodec codec = getTestCodec();
-		String text = "string";
-		int code = codec.createCode();
-		codec.ensureCapacity(code);
-		EventCodec.Word word = new EventCodec.Word(code, text);
-		codec.alphabet[code] = word;
-		assertEquals("word equals getString()", word.toString(), codec.getString(code));
-	}
+    /**
+     * Test for getFirstLiteral().
+     */
+    public void testGetFirstLiteral() {
+        TestCodec codec = getTestCodec();
+        assertTrue(
+            "first literal <= created code",
+            codec.getFirstLiteral() <= codec.createCode());
+    }
 
-	/**
-	 * Test for getUCS4(int).
-	 */
-	public void testGetUCS4() {
-		TestCodec codec = getTestCodec();
-		String text = "string";
-		int ucs4[] = EventCodec.toUCS4(text);
-		int code = codec.createCode();
-		codec.ensureCapacity(code);
-		EventCodec.Word word = new EventCodec.Word(code, ucs4);
-		codec.alphabet[code] = word;
-		assertEquals("word equals getUCS4()", word.getUCS4(), codec.getUCS4(code));
-	}
+    /**
+     * Test for getString(int).
+     */
+    public void testGetString() {
+        TestCodec codec = getTestCodec();
+        String text = "string";
+        int code = codec.createCode();
+        codec.ensureCapacity(code);
+        EventCodec.Word word = new EventCodec.Word(code, text);
+        codec.alphabet[code] = word;
+        assertEquals(
+            "word equals getString()",
+            word.toString(),
+            codec.getString(code));
+    }
 
-	/**
-	 * Test for createCode().
-	 */
-	public void testCreateCode() {
-		TestCodec codec = getTestCodec();
-		int code1 = codec.createCode();
-		int code2 = codec.createCode();
-		assertTrue("code1 != code1", code1 != code2);
-	}
+    /**
+     * Test for getUCS4(int).
+     */
+    public void testGetUCS4() {
+        TestCodec codec = getTestCodec();
+        String text = "string";
+        int ucs4[] = EventCodec.toUCS4(text);
+        int code = codec.createCode();
+        codec.ensureCapacity(code);
+        EventCodec.Word word = new EventCodec.Word(code, ucs4);
+        codec.alphabet[code] = word;
+        assertEquals(
+            "word equals getUCS4()",
+            word.getUCS4(),
+            codec.getUCS4(code));
+    }
 
-	/**
-	 * Test for ensureCapacity(int).
-	 */
-	public void testEnsureCapacity() {
-		TestCodec codec = getTestCodec();
-		int code1 = codec.createCode();
-		codec.ensureCapacity(code1);
-		try {
-			codec.getWord(code1);
-		} catch (Exception ex) {
-			fail("ensureCapacity: "+ex);
-		}
-	}
+    /**
+     * Test for createCode().
+     */
+    public void testCreateCode() {
+        TestCodec codec = getTestCodec();
+        int code1 = codec.createCode();
+        int code2 = codec.createCode();
+        assertTrue("code1 != code1", code1 != code2);
+    }
 
-	/**
-	 * Test for toUCS4(String).
-	 */
-	public void testToUCS4()
-	throws UnsupportedEncodingException {
-		String text = "string";
-		int ucs4[] = toUCS4(text);
-		assertTrue("ucs4(text) equals toUCS4(text))",
-			Arrays.equals(ucs4, EventCodec.toUCS4(text)));
-	}
-	
-	public static int[] toUCS4(String text)
-	throws UnsupportedEncodingException {
-		UTF8Decoder decoder = new UTF8Decoder();
-		decoder.setBuffer(text.getBytes("UTF-8"));
-		return decoder.decodeUCS4(null, 0, text.length());
-	}
+    /**
+     * Test for ensureCapacity(int).
+     */
+    public void testEnsureCapacity() {
+        TestCodec codec = getTestCodec();
+        int code1 = codec.createCode();
+        codec.ensureCapacity(code1);
+        try {
+            codec.getWord(code1);
+        } catch (Exception ex) {
+            fail("ensureCapacity: " + ex);
+        }
+    }
 
-	public static class TestCodec extends EventCodec {
-	}
+    /**
+     * Test for toUCS4(String).
+     */
+    public void testToUCS4() throws UnsupportedEncodingException {
+        String text = "string";
+        int ucs4[] = toUCS4(text);
+        assertTrue(
+            "ucs4(text) equals toUCS4(text))",
+            Arrays.equals(ucs4, EventCodec.toUCS4(text)));
+    }
+
+    public static int[] toUCS4(String text)
+        throws UnsupportedEncodingException {
+        UTF8Decoder decoder = new UTF8Decoder();
+        decoder.setBuffer(text.getBytes("UTF-8"));
+        return decoder.decodeUCS4(null, 0, text.length());
+    }
+
+    public static class TestCodec extends EventCodec {
+    }
 }
--- fraglets/unit/net/sourceforge/fraglets/codec/SAXCodecTest.java
+++ fraglets/unit/net/sourceforge/fraglets/codec/SAXCodecTest.java
@@ -27,75 +27,72 @@ import junit.framework.TestSuite;
  */
 public abstract class SAXCodecTest extends TestCase {
 
-	/** Test XML input. */
+    /** Test XML input. */
     public static final String TEST_INPUT =
-    	"<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
-    	"<root>\n" +
-    	" <sub id=\"sub1\">Some text</sub>\n" +
-    	" <sub id=\"sub2\">Some more text</sub>\n" +
-    	"</root>\n";
-    
-	/**
-	 * @see junit.framework.TestCase#TestCase(String)
-	 */
+        "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
+            + "<root>\n"
+            + " <sub id=\"sub1\">Some text</sub>\n"
+            + " <sub id=\"sub2\">Some more text</sub>\n"
+            + "</root>\n";
+
+    /**
+     * @see junit.framework.TestCase#TestCase(String)
+     */
     public SAXCodecTest(String testName) {
         super(testName);
     }
 
-	public InputSource toInputSource(String input) throws IOException {
-    	return new InputSource(new StringReader(input));
+    public InputSource toInputSource(String input) throws IOException {
+        return new InputSource(new StringReader(input));
     }
-    
 
-    
-    public class EventOutput extends UTF8XMLWriter
-    implements DocumentHandler {
-    	private DocumentHandler delegate;
-    	
+    public class EventOutput extends UTF8XMLWriter implements DocumentHandler {
+        private DocumentHandler delegate;
+
         public EventOutput() {
             super(new ByteArrayOutputStream());
         }
-        
+
         public byte[] getBytes() {
-        	return ((ByteArrayOutputStream)lock).toByteArray();
+            return ((ByteArrayOutputStream) lock).toByteArray();
         }
-        
+
         public void reset() {
-        	((ByteArrayOutputStream)lock).reset();
-        	setDelegate(null);
+            ((ByteArrayOutputStream) lock).reset();
+            setDelegate(null);
         }
 
-		public void setDelegate(DocumentHandler delegate) {
-			this.delegate = delegate;
-		}
+        public void setDelegate(DocumentHandler delegate) {
+            this.delegate = delegate;
+        }
 
-		public DocumentHandler getDelegate() {
-			return delegate;
-		}
-        
+        public DocumentHandler getDelegate() {
+            return delegate;
+        }
+
         public void setDocumentLocator(Locator locator) {
-        	if (delegate != null) {
-        		delegate.setDocumentLocator(locator);
-        	}
+            if (delegate != null) {
+                delegate.setDocumentLocator(locator);
+            }
         }
-        
+
         public void startDocument() throws SAXException {
-        	if (delegate != null) {
-        		delegate.startDocument();
-        	}
+            if (delegate != null) {
+                delegate.startDocument();
+            }
         }
-        
+
         public void endDocument() throws SAXException {
-        	if (delegate != null) {
-        		delegate.endDocument();
-        	}
+            if (delegate != null) {
+                delegate.endDocument();
+            }
         }
-        
+
         public void startElement(String str, AttributeList attrs)
-        throws SAXException {
-        	if (delegate != null) {
-        		delegate.startElement(str, attrs);
-        	}
+            throws SAXException {
+            if (delegate != null) {
+                delegate.startElement(str, attrs);
+            }
             try {
                 startElement(str);
                 int end = attrs.getLength();
@@ -103,53 +100,53 @@ public abstract class SAXCodecTest extends TestCase {
                     attribute(attrs.getName(i), attrs.getValue(i));
                 }
             } catch (IOException ex) {
-        		throw new UndeclaredThrowableException(ex);
+                throw new UndeclaredThrowableException(ex);
             }
         }
-        
+
         public void characters(char[] values, int param, int param2)
-        throws SAXException {
-        	if (delegate != null) {
-        		delegate.characters(values, param, param2);
-        	}
+            throws SAXException {
+            if (delegate != null) {
+                delegate.characters(values, param, param2);
+            }
             try {
                 write(values, param, param2);
             } catch (IOException ex) {
-        		throw new UndeclaredThrowableException(ex);
+                throw new UndeclaredThrowableException(ex);
             }
         }
-        
+
         public void ignorableWhitespace(char[] values, int param, int param2)
-        throws SAXException {
-        	if (delegate != null) {
-        		delegate.ignorableWhitespace(values, param, param2);
-        	}
+            throws SAXException {
+            if (delegate != null) {
+                delegate.ignorableWhitespace(values, param, param2);
+            }
             try {
                 write(values, param, param2);
             } catch (IOException ex) {
-        		throw new UndeclaredThrowableException(ex);
+                throw new UndeclaredThrowableException(ex);
             }
         }
-        
+
         public void processingInstruction(String str, String str1) {
             try {
-	        	if (delegate != null) {
-	        		delegate.processingInstruction(str, str1);
-	        	}
+                if (delegate != null) {
+                    delegate.processingInstruction(str, str1);
+                }
                 super.processingInstruction(str, str1);
             } catch (Exception ex) {
-        		throw new UndeclaredThrowableException(ex);
+                throw new UndeclaredThrowableException(ex);
             }
         }
-        
+
         public void endElement(String str) {
             try {
-	        	if (delegate != null) {
-		        		delegate.endElement(str);
-	        	}
+                if (delegate != null) {
+                    delegate.endElement(str);
+                }
                 super.endElement(str);
             } catch (Exception ex) {
-        		throw new UndeclaredThrowableException(ex);
+                throw new UndeclaredThrowableException(ex);
             }
         }
     }
